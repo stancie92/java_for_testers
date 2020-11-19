43ccdf58c8175246d5ec63f772c2a4b03716be3b
stancie92 <stanislawciesielka@gmail.com>
2020-11-19 21:41:09 +0100

    Delete AppFile and add first homework (after first paragraph).

--- src/Excercise1.java
+++ src/Excercise1.java
@@ -1,2 +1,28 @@
 public class Excercise1 {
+
+    public static void main(String[] args) {
+
+        //subpoint1
+        int first = 53;
+        int second = 51;
+        int three = 57978;
+        int four = -82;
+
+        //subpoints 2,3,4
+
+        int sumaA = first +second;
+        int sumaB = three + four;
+
+        if(sumaA % 2 ==0 && sumaB %2 ==0){
+            System.out.println("Both sum are even.");
+        }
+        else{
+            System.out.println("One sum isn't even.");
+        }
+
+
+
+    }
+
+
 }
