851acdca8a55945dd6981aaeabd5e1b29cd58356
stancie92 <stanislawciesielka@gmail.com>
2020-11-21 12:14:44 +0100

    Added third homework

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
+import Other.Bug;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Bug todayBug = new Bug("'Issue with submit login form'", "stancie@example.pl", 5);
-
+        todayBug.setDescription("adsdf");
+        todayBug.setAdressEmail("dada.");
+        todayBug.setPiority(-1);
+        todayBug.setAdressEmail("stanciesi@onet.pl");
         todayBug.getInformationAboutBug();
         todayBug.getEmailOfAuthor();
-        todayBug.getPiority();
+        int piority = todayBug.getPiority();
+        System.out.println(piority);
         todayBug.getStatus();
     }
 }
