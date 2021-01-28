bad3cc9aee77d3de1596f0473475f47f2fbef837
Stanisław Ciesielka <stanislawciesielka@gmail.com>
2021-01-28 23:14:59 +0100

    Add homework about Collections

---
+++ src/utilsw/Main.java
@@ -0,0 +1,20 @@
+import Other.Bug;
+import Other.User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<User> users = new ArrayList<>();
+
+        users.add()
+
+
+
+    }
+}
